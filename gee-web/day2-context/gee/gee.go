1469fb1043cfc64b8aade27f4f71d0f6e0986996
hucy <hucy@gegolab.com>
2021-03-14 15:49:08 +0800

    1. 将路由(router)独立出来，方便之后增强。 2. 设计上下文(Context)，封装 Request 和 Response ，提供对 JSON、HTML 等返回类型的支持。

---
+++ gee-web/day2-context/gee/gee.go
@@ -0,0 +1,37 @@
+package gee
+
+import "net/http"
+
+// 重点: *** 此处定义的func 为自定义的结构方法，会在调用和context中使用代替 handler http.HandlerFunc
+type HandlerFunc func(*Context)
+
+type Engine struct {
+	router *router
+}
+
+func New() *Engine {
+	return &Engine{
+		router: newRouter(),
+	}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(add string) (err error) {
+	return http.ListenAndServe(add, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
+}
